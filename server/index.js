e7d311a7b87032e9323b67e7fbfc6b848162d70a
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-08-24 03:14:38 +0600

    fixed few issues

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
-
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,13 +15,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-    "mongodb+srv://rafiuislam:rafiuislam123@cluster0.qiddp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL, {
+    .connect(process.env.CONNECTION_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
